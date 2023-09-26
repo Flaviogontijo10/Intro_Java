5ec6b453cf294e48c96d32820b594b9245bd8317
Flavio <flaviogontijofigueira@gmail.com>
2023-09-26 09:45:19 -0300

    Primeiro exemplo java

---
+++ src/main/java/intro/Medidas.java
@@ -0,0 +1,17 @@
+// 1 - Pacote
+
+package intro;
+
+// 2 - Referência as bibliotecas
+
+// 3 - Classe
+public class Medidas {
+    // 3.1 - Atributos - Características
+
+    // 3.2 - Métodos e Funções
+  public static void main (String[] args){
+      System.out.println ("teste");
+  }
+
+
+}
